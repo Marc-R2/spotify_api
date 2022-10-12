82536fd98c7f8ca7e3f31d714f425bee68c21ff7
Marc Renken <marc.renken@bitbot.eu>
2022-10-12 18:29:17 +0200

    Markets Endpoints

--- lib/src/endpoints/endpoint_markets.dart
+++ lib/src/endpoints/endpoint_markets.dart
@@ -3,7 +3,28 @@ part of 'spotify_api_endpoint.dart';
 class SpotifyApiMarkets extends SpotifyApiEndpoint {
   SpotifyApiMarkets({required super.api});
 
-  Future<String?> getMarket(String id) async {
-    // return await _api.getRequest(endpoint: 'markets/$id');
+  /// Method: GET
+  /// Endpoint: /v1/markets
+  /// Description: Get a List of Markets
+  Future<Success<List<String>>> getMarkets() async {
+    const fail = Success<List<String>>(success: false);
+    if (_api.hasScopes(const SpotifyApiScopes())) {
+      final json = await _api.getRequest(
+        endpoint: '/v1/markets',
+      );
+      if (json == null) return fail;
+      try {
+        final markets = json['items'] != null
+            ? List<String>.from(
+                (json['items'] as List).map((x) => x.toString()),
+              )
+            : <String>[];
+
+        return Success(data: markets);
+      } catch (e, trace) {
+        print('$e => $trace');
+      }
+    }
+    return fail;
   }
 }
